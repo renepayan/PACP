a38852d9d8884f204e0d43af458c672ba7256180
René Payán Téllez <renepayan62@gmail.com>
2020-10-01 14:54:12 -0400

    terminado

--- Clase 01-10-2020/Modulos del proyecto simple/procesamiento.c
+++ Clase 01-10-2020/Modulos del proyecto simple/procesamiento.c
@@ -6,7 +6,7 @@
 #include <math.h>
 #include "defs.h"
 
-void genera_seno(float datos[]){
+void genera_seno(float seno[]){
 	float f = 1000, fs = 45000;
 	register int n;
 	for( n = 0;n < MUESTRAS; n++){
