9a07451d082896bc4b12378ac289dc4b2d12d694
René Payán Téllez <payantellez1g@hotmail.com>
2021-01-06 07:09:04 -0600

    primera prueba

--- Tarea 7 - Filtro Gaussiano/principal.c
+++ Tarea 7 - Filtro Gaussiano/principal.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <math.h>
 #include "imagen.h"
-#define DIMASK 5
+#define DIMASK 3
 #define VARIANZA 1
 void RGBToGray( unsigned char *imagenRGB, unsigned char *imagenGray, uint32_t width, uint32_t height );
 void GrayToRGB( unsigned char *imagenRGB, unsigned char *imagenGray, uint32_t width, uint32_t height );
@@ -18,27 +18,39 @@ int main( )
 	bmpInfoHeader info;
 	unsigned char *imagenRGB, *imagenGray, *imagenFiltrada;
 
-	imagenRGB = abrirBMP("huella1.bmp", &info );
-
+	imagenRGB = abrirBMP("linux.bmp", &info );
 	displayInfo( &info );
-	imagenGray = reservarMemoria( info.width, info.height );
-	imagenFiltrada = reservarMemoria( info.width, info.height );
-	memset( imagenFiltrada, 255, info.width*info.height );
-	printf("Reservo memoria\n");
-	RGBToGray( imagenRGB, imagenGray, info.width, info.height );
-	printf("Creo el blanco y negro\n");
-	//brilloImagen( imagenGray, info.width, info.height );
-	filtroGaussiano( imagenGray, imagenFiltrada, info.width, info.height );
-	printf("Creo el filtro\n");
-	//GrayToRGB( imagenRGB, imagenFiltrada, info.width, info.height );
-	//printf("Lo regreso a color\n");
-	/*
-	guardarBMP("huella1final.bmp", &info, imagenRGB );
 
+	imagenGray = reservarMemoria( info.width, info.height );
+    imagenFiltrada = reservarMemoria( info.width, info.height );
+
+    memset( imagenGray, 255, info.width*info.height );
+
+    //RGBToGray( imagenRGB, imagenGray, info.width, info.height );
+
+    //filtroPB(imagenGray, imagenFiltrada, info.width, info.height);
+
+    //GrayToRGB( imagenRGB ,imagenGray, info.width, info.height );
+
+    guardarBMP("linux1.bmp", &info, imagenRGB );
+
+    free( imagenFiltrada );
+	free(imagenGray);
+    free(imagenRGB);
+    return 0;
+
+
+
+
+
+	//filtroPB( imagenGray, imagenFiltrada, info.width, info.height );
+
+
+
+    guardarBMP("linux1.bmp", &info, imagenRGB );
 	free( imagenRGB );
 	free( imagenGray );
-	free( imagenFiltrada );
-	*/
+
 	return 0;
 }
 
@@ -68,30 +80,36 @@ void filtroPB( unsigned char *imagenG, unsigned char *imagenF, uint32_t width, u
 }
 
 void filtroGaussiano( unsigned char *imagenG, unsigned char *imagenF, uint32_t width, uint32_t height ){
-	
+
 	register int i, j, x, y, xm, ym;
 	int indicem, indicei, conv;
-	double **mascara;
-	mascara = (double**)malloc(sizeof(double*)*DIMASK);
+	double *mascara;
+	mascara = (double*)malloc(sizeof(double)*DIMASK);
 	for(i = 0; i < DIMASK; i++){
-		mascara[i] = (double*)malloc(sizeof(double)*DIMASK);
-		for(j = 0; j < DIMASK; j++){			
+		for(j = 0; j < DIMASK; j++){
 			double division = (double)2.0 * VARIANZA;
 			double razonX = (double)( (-DIMASK/2+i) * (-DIMASK/2+i) );
 			double razonY = (double)( (-DIMASK/2+j) * (-DIMASK/2+j) );
-			double contenido = -((razonX+razonY)/division);			
-			mascara[i][j] = ((double)1.0/(double)(2*M_PI*VARIANZA))*exp(contenido);
+			double contenido = -((razonX+razonY)/division);
+			mascara[DIMASK*i+j] = ((double)1.0/(double)(2*M_PI*VARIANZA))*exp(contenido);
 		}
+	}
+	for(i = 0; i < DIMASK; i++){
+		for(j = 0; j < DIMASK; j++){
+			printf("%f ",mascara[DIMASK*i+j]);
+		}
+		printf("\n");
 	}
 	for( y = 0; y <= height-DIMASK; y++ )
 		for( x = 0; x <= width-DIMASK; x++ )
-		{									
+		{
+			indicem = 0;
 			conv = 0;
 			for( ym = 0; ym < DIMASK; ym++ )
 				for( xm = 0; xm < DIMASK; xm++ )
-				{									
+				{
 					indicei = (y+ym)*width + (x+xm);
-					conv += imagenG[indicei] * mascara[ym][xm];
+					conv += ((double)imagenG[indicei] * mascara[indicem++]);
 				}
 			conv = conv / 9;
 			indicei = (y+1)*width + (x+1);
@@ -122,59 +140,16 @@ void GrayToRGB( unsigned char *imagenRGB, unsigned char *imagenGray, uint32_t wi
 		imagenRGB[indiceRGB+2] = imagenGray[indiceGray];
 	}
 }
-void RGBToGray( unsigned char *imagenRGB, unsigned char *imagenGray, uint32_t width, uint32_t height )
-{
+void RGBToGray( unsigned char *imagenRGB, unsigned char *imagenGray, uint32_t width, uint32_t height ){
 	unsigned char nivelGris;
 	int indiceRGB, indiceGray;
 
 	for( indiceGray = 0, indiceRGB = 0; indiceGray < (height*width); indiceGray++, indiceRGB += 3 )
 	{
-	//	nivelGris = (imagenRGB[indiceRGB] + imagenRGB[indiceRGB+1] + imagenRGB[indiceRGB+2]) / 3;
 		nivelGris = (30*imagenRGB[indiceRGB] + 59*imagenRGB[indiceRGB+1] + 11*imagenRGB[indiceRGB+2]) / 100;
-		// 0.30 = 30 / 100
-		// 0.59 = 59 / 100
-		// 0.11 = 11 / 100
 		imagenGray[indiceGray] = nivelGris;
 	}
 }
-
-/*
-void GrayToRGB( unsigned char *imagenRGB, unsigned char *imagenGray, uint32_t width, uint32_t height )
-{
-	register int x, y;
-	int indiceRGB, indiceGray;
-
-	for( y = 0; y < height; y++ )
-		for( x = 0; x < width; x++ )
-		{
-			indiceGray = (y*width + x);
-			indiceRGB =  indiceGray * 3;
-			imagenRGB[indiceRGB]   = imagenGray[indiceGray];
-			imagenRGB[indiceRGB+1] = imagenGray[indiceGray];
-			imagenRGB[indiceRGB+2] = imagenGray[indiceGray];
-		}
-}
-
-void RGBToGray( unsigned char *imagenRGB, unsigned char *imagenGray, uint32_t width, uint32_t height )
-{
-	register int x, y;
-	unsigned char nivelGris;
-	int indiceRGB, indiceGray;
-
-	for( y = 0; y < height; y++ )
-		for( x = 0; x < width; x++ )
-		{
-			indiceGray = (y*width + x);
-			indiceRGB =  indiceGray * 3;
-		//	nivelGris = (imagenRGB[indiceRGB] + imagenRGB[indiceRGB+1] + imagenRGB[indiceRGB+2]) / 3;
-			nivelGris = (30*imagenRGB[indiceRGB] + 59*imagenRGB[indiceRGB+1] + 11*imagenRGB[indiceRGB+2]) / 100;
-		// 0.30 = 30 / 100
-		// 0.59 = 59 / 100
-		// 0.11 = 11 / 100
-			imagenGray[indiceGray] = nivelGris;
-		}
-}*/
-
 unsigned char * reservarMemoria( uint32_t width, uint32_t height )
 {
 	unsigned char *imagen;
