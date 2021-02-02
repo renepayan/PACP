009deafff5ab4f49e02e568867ee27f9b5c6327e
René Payán Téllez <payantellez1g@hotmail.com>
2021-02-02 07:53:31 -0600

    avance del servidor

--- Tarea 9 - Proyecto Final/servidor/procesamiento.h
+++ Tarea 9 - Proyecto Final/servidor/procesamiento.h
@@ -4,6 +4,6 @@
 void RGBToGray( unsigned char *imagenRGB, unsigned char *imagenGray, uint32_t width, uint32_t height );
 void GrayToRGB( unsigned char *imagenRGB, unsigned char *imagenGray, uint32_t width, uint32_t height );
 void llenarMascara(double *mascara);
-void atiendeCliente( int cliente_sockfd, unsigned char *imagenGray, int tamImagen );
+void atiendeCliente( int cliente_sockfd, bmpInfoHeader *info, unsigned char *imagenGray, int tamImagen );
 int iniServidor( );
 #endif
